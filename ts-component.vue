fa775f163195ed406073bf6b618bb8efbeff9eaf
Bracaval Elias <45099340+yatoogamii@users.noreply.github.com>
2020-06-19 11:52:04 +0200

    Create ts-component.vue

---
+++ ts-component.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Long-form v-model example</p>
+    <input :value="myDataProperty" @input="updateMyProperty($event)"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+  // Data property
+  myDataProperty: string;
+
+  // Lifecycle hook
+  mounted () {
+    this.myDataProperty = 'Boop'
+  }
+
+  // Component method
+  updateMyProperty ($event) {
+    this.myDataProperty = $event.target.value
+  }
+}
+</script>
